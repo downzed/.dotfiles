b69cf3ba726c2b1343356c894af1566bc8bbf6ca
Ziv Zerr <downzed@gmail.com>
2023-04-25 22:42:13 +0300

    Update installer.py

--- installer.py
+++ installer.py
@@ -2,7 +2,6 @@ import subprocess
 import os
 
 
-subprocess.run(['curl', '-o-', 'https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh', '|', 'bash'])
 
 
 # Install the Nala package
@@ -11,6 +10,10 @@ subprocess.run(['sudo', 'apt', 'install', 'nala', '-y'])
 
 # Install the required packages using Nala
 subprocess.run(['sudo', 'nala', 'install', 'sway', 'neovim', 'waybar', 'wofi', 'curl', 'git', 'wget', 'zsh', '-y'])
+
+# Install nvm
+subprocess.run(['curl', '-o-', 'https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh', '|', 'bash'])
+
 
 # Install oh-my-zsh
 os.system('sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"')
