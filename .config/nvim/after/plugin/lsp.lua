a3848784db4fb644633adacf69facef7553d480d
Ziv Zerr <downzed@gmail.com>
2023-09-12 13:36:00 +0300

    lsps, cmp, cleanups

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -2,12 +2,10 @@ local lsp = require('lsp-zero')
 local lspconfig = require('lspconfig')
 local mason = require('mason')
 local masonconfig = require('mason-lspconfig')
--- local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+local cmp = require('cmp')
 
 
 lsp.preset('recommended')
-mason.setup({})
--- lsp.nvim_workspace()
 
 local servers = {
   "tsserver",
@@ -18,16 +16,23 @@ local servers = {
   "rust_analyzer"
 }
 
--- lsp.nvim_workspace()
--- lsp.ensure_installed(servers)
-
+mason.setup({})
 masonconfig.setup({
   ensure_installed = servers,
   automatic_installation = true,
   handlers = {
     lsp.default_setup,
+    lua_ls = function()
+      local lua_opts = lsp.nvim_lua_ls()
+      lspconfig.lua_ls.setup(lua_opts)
+    end
   }
 })
+
+local function eslint_config_exists()
+  local eslintrc = vim.fn.glob(".eslintrc*", 0, 1)
+  return not vim.tbl_isempty(eslintrc)
+end
 
 local on_attach = function(_, bufnr)
   local opts = { buffer = bufnr }
@@ -39,17 +44,23 @@ local on_attach = function(_, bufnr)
   -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
   vim.keymap.set('n', 'H', vim.lsp.buf.hover, opts)
   vim.keymap.set('n', 'K', vim.lsp.buf.code_action, opts)
-  vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, opts)
+
+  if not eslint_config_exists() then
+    lsp.buffer_autoformat()
+    vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, opts)
+  else
+    vim.keymap.set('n', '<leader>f', ':EslintFixAll<cr>', opts)
+  end
 end
 
 lsp.on_attach(on_attach)
--- lsp.skip_server_setup({ 'rust_analyzer' })
 
 lsp.format_on_save({
   servers = {
     ['lua_ls'] = { 'lua' },
     ['rust_analyzer'] = { 'rust' },
     ['pyright'] = { 'python' },
+    -- TODO: I hate work
     ['eslint'] = {
       'javascript.js',
       'javascript.jsx',
@@ -63,42 +74,18 @@ lsp.format_on_save({
   }
 })
 
-local function eslint_config_exists()
-  local eslintrc = vim.fn.glob(".eslintrc*", 0, 1)
-  print("eslintrc > provided")
-  return not vim.tbl_isempty(eslintrc)
-end
-
-lspconfig.eslint.setup({
-  single_file_support = false,
-  root_dir = function()
-    return vim.fn.getcwd()
-  end,
-  on_attach = function(_, bufnr)
-    local opts = { buffer = bufnr }
-
-    lsp.default_keymaps(opts)
-
-    if not eslint_config_exists() then
-      return nil
-    end
-
-    vim.keymap.set('n', '<leader>f', ':EslintFixAll<cr>', opts)
-
-    vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<cr>', opts)
-    vim.keymap.set('n', 'gi', '<cmd>Telescope lsp_implementations<cr>', opts)
-    vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', opts)
-    -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', 'H', vim.lsp.buf.hover, opts)
-    -- vim.keymap.set('n', 'K', vim.lsp.buf.code_action, opts)
-  end,
-  settings = {
-    packageManager = "yarn",
-  }
-})
-
 vim.diagnostic.config({
   signs = true
 })
 
+cmp.setup({
+  formatting = lsp.cmp_format(),
+  mapping = cmp.mapping.preset.insert({
+    -- accept first option
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    -- scroll up and down the documentation window
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+  }),
+})
 lsp.setup()
