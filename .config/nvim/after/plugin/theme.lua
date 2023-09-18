d3620ee43ad597a4671408e8502b53ff0ec44b01
Ziv Zerr <downzed@gmail.com>
2023-09-18 18:35:51 +0300

    find and replace kb

--- .config/nvim/after/plugin/theme.lua
+++ .config/nvim/after/plugin/theme.lua
@@ -17,6 +17,9 @@ function SetColorscheme(color)
   vim.cmd.colorscheme(color)
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+  vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })
 end
 
 SetColorscheme()
--- .config/nvim/lua/downzed/lazy.lua
+++ .config/nvim/lua/downzed/lazy.lua
@@ -92,15 +92,15 @@ local plugins = {
 
   -- ui, notifications & messages
   {
-    "folke/noice.nvim",
-    event = "VeryLazy",
+    'folke/noice.nvim',
+    event = 'VeryLazy',
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
+      'MunifTanjim/nui.nvim',
       -- OPTIONAL:
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
+      'rcarriga/nvim-notify',
     }
   },
   {
