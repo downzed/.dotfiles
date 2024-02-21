7223d10ec6f6cfaa2d52cb7cf57094120f1b203c
Ziv Zerr <downzed@gmail.com>
2024-02-21 18:28:14 +0100

    markdown preview

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -1,11 +1,17 @@
 return {
-	"alexghergh/nvim-tmux-navigation",
-	"ThePrimeagen/vim-be-good", -- practive vim
-	"tpope/vim-commentary", -- comment/uncomments
-	{
-		"folke/which-key.nvim",
-		config = function()
-			require("which-key").setup()
-		end,
-	},
+  "alexghergh/nvim-tmux-navigation",
+  "ThePrimeagen/vim-be-good", -- practive vim
+  "tpope/vim-commentary",    -- comment/uncomments
+  {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup()
+    end,
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  }
 }
