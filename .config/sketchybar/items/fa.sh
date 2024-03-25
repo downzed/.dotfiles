90067c79a07bd0d6957fd3c1dd349579ebb706fd
Ziv Zerr <downzed@gmail.com>
2024-03-25 13:09:49 +0100

    lsp-zero.. again

---
+++ .config/sketchybar/items/fa.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sketchybar --add item chevron left \
+           --set chevron icon= label.drawing=off \
+           --add item front_app left \
+           --set front_app icon.drawing=off script="$PLUGIN_DIR/front_app.sh" \
+           --subscribe front_app front_app_switched
+
