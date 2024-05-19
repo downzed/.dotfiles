e92bc196ec93a80096edaa535f88149057a06a14
Ziv Zerr <downzed@gmail.com>
2024-05-19 12:47:05 +0200

    feat: adding neorg

--- .config/nvim/lua/downzed/plugins/themes.lua
+++ .config/nvim/lua/downzed/plugins/themes.lua
@@ -38,40 +38,12 @@ return {
           transparency = true,
         },
       })
-      -- ApplyTheme('rose-pine-main')
     end,
   },
   {
-    "loctvl842/monokai-pro.nvim",
+    "rebelot/kanagawa.nvim", -- neorg needs a colorscheme with treesitter support
     config = function()
-      require("monokai-pro").setup({
-        transparent_background = true,
-        terminal_colors = true,
-        devicons = true, -- highlight the icons of `nvim-web-devicons`
-        styles = {
-          comment = { italic = true },
-          keyword = { italic = true },       -- any other keyword
-          type = { italic = true },          -- (preferred) int, long, char, etc
-          storageclass = { italic = true },  -- static, register, volatile, etc
-          structure = { italic = true },     -- struct, union, enum, etc
-          parameter = { italic = true },     -- parameter pass in function
-          annotation = { italic = true },
-          tag_attribute = { italic = true }, -- attribute of tag in reactjs
-        },
-        -- filter = "machine",                  -- classic | octagon | pro | machine | ristretto | spectrum
-        background_clear = {
-          "float_win",
-          -- "toggleterm",
-          -- "telescope",
-          "which-key",
-          "renamer",
-          "notify",
-          -- "nvim-tree",
-          "neo-tree",
-          -- "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
-        }, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
-      })
-      ApplyTheme('monokai-pro-machine')
-    end
-  }
+      ApplyTheme("kanagawa-dragon")
+    end,
+  },
 }
