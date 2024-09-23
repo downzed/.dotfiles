f343304164dfbce4483c7adc9452e098584b39d3
Ziv Zerr <downzed@gmail.com>
2024-09-23 10:35:48 +0200

    better zsh history

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -3,7 +3,6 @@ alias cls="clear"
 alias vi="fd --type f --hidden --exclude .git | fzf-tmux --height=50% --reverse --preview 'bat {}' | xargs nvim"
 alias g="git branch --list | fzf --height=50% --reverse | xargs git checkout" 
 alias sc="bat package.json | jq -r '.scripts | keys[]' | fzf-tmux --height=65% --reverse --border | xargs yarn"
-alias ..="cd .."
 alias top="bpytop"
 alias py="python3"
 alias sleepnow="pmset sleepnow"
@@ -20,13 +19,6 @@ alias lst='eza --tree'
 alias dp='docker ps --format "table {{.ID}}\t{{.Names}}\t\t{{.Status}}\t{{.Ports}}"' ## docker ps table
 alias dpe='docker exec -it $(docker ps --format "{{.Names}}" | fzf) bash' ## docker exec fzf  
 alias dr='docker-compose down; cls; docker-compose up --watch' ## docker restart 
-
-alias eza='eza --icons'
-alias ls='eza'
-alias ll='eza -l'
-alias la='eza -a'
-alias lsa='eza -la'
-alias lst='eza --tree'
 
 alias pn='pnpm'
 
