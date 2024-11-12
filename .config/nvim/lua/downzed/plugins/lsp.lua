70507f06dc97a9704654d3a284e30aa95adf20f7
Ziv Zerr <downzed@gmail.com>
2024-11-13 00:14:06 +0100

    tailwindcss, lsp, stylua

--- .config/nvim/lua/downzed/plugins/lsp.lua
+++ .config/nvim/lua/downzed/plugins/lsp.lua
@@ -10,18 +10,18 @@ return {
         "j-hui/fidget.nvim",
         opts = {
           notification = {
-            override_vim_notify = true,  -- Automatically override vim.notify() with Fidget
+            override_vim_notify = true, -- Automatically override vim.notify() with Fidget
             window = {
               normal_hl = "NormalFloat", -- Base highlight group in the notification window
-              winblend = 45,             -- Background color opacity in the notification window
-              border = "none",           -- Border around the notification window
-              zindex = 45,               -- Stacking priority of the notification window
-              max_width = 0,             -- Maximum width of the notification window
-              max_height = 0,            -- Maximum height of the notification window
-              x_padding = 1,             -- Padding from right edge of window boundary
-              y_padding = 0,             -- Padding from bottom edge of window boundary
-              align = "bottom",          -- How to align the notification window
-              relative = "editor",       -- What the notification window position is relative to
+              winblend = 45, -- Background color opacity in the notification window
+              border = "none", -- Border around the notification window
+              zindex = 45, -- Stacking priority of the notification window
+              max_width = 0, -- Maximum width of the notification window
+              max_height = 0, -- Maximum height of the notification window
+              x_padding = 1, -- Padding from right edge of window boundary
+              y_padding = 0, -- Padding from bottom edge of window boundary
+              align = "bottom", -- How to align the notification window
+              relative = "editor", -- What the notification window position is relative to
             },
           },
         },
@@ -41,7 +41,7 @@ return {
         },
       }
       local capabalities =
-          vim.tbl_deep_extend("force", {}, vim_lsp_capabilities, cmp_lsp_capabilities)
+        vim.tbl_deep_extend("force", {}, vim_lsp_capabilities, cmp_lsp_capabilities)
 
       require("mason").setup()
       require("mason-lspconfig").setup({
@@ -53,6 +53,7 @@ return {
           "clangd",
           "html",
           "cssls",
+          "tailwindcss",
           "pyright",
           "pylsp",
           "jsonls",
