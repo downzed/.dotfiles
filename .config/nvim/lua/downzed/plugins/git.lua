16ca070b425c89acd5bb13d9b740357b6fa1d018
Ziv Zerr <downzed@gmail.com>
2025-03-03 12:29:24 +0100

    git

--- .config/nvim/lua/downzed/plugins/git.lua
+++ .config/nvim/lua/downzed/plugins/git.lua
@@ -1,3 +1,12 @@
+local map = function(key, cmd, desc)
+  local opts = {
+    silent = true,
+    desc = 'Git: ' .. desc,
+  }
+
+  vim.keymap.set('n', key, cmd, opts)
+end
+
 return {
   'tpope/vim-fugitive',
   {
@@ -14,26 +23,16 @@ return {
           changedelete = { text = '~' },
           untrack = { text = '·' },
         },
-
-        on_attach = function(bufnr)
-          local map = function(key, cmd, desc)
-            vim.keymap.set('n', key, cmd, {
-              silent = true,
-              desc = 'Git: ' .. desc,
-              buffer = bufnr,
-            })
-          end
-
-          map('<leader>gs', '<cmd>Gitsigns<cr>', '[G]it [S]igns')
-          map('<leader>gf', '<cmd>Git<cr>', '[G]it [F]ugitive')
-          map('<leader>gss', gs.stage_hunk, '[G]it[s]igns [S]tage Hunk')
-          map('<leader>gsx', gs.reset_hunk, '[G]it[s]igns [x] Reset Hunk')
-          map('<leader>gsp', gs.preview_hunk, '[G]it[s]igns [P]review hunk')
-          map('<leader>gs]', gs.next_hunk, '[G]it[s]igns ] next hunk')
-          map('<leader>gs[', gs.prev_hunk, '[G]it[s]igns [ previous hunk')
-          map('<leader>tl', gs.toggle_current_line_blame, '[T]oggle [L]ine blame')
-        end,
       })
+
+      map('<leader>gs', '<cmd>Gitsigns<cr>', '[G]it [S]igns')
+      map('<leader>gf', '<cmd>Git<cr>', '[G]it [F]ugitive')
+      map('<leader>gss', gs.stage_hunk, '[G]it[s]igns [S]tage Hunk')
+      map('<leader>gsx', gs.reset_hunk, '[G]it[s]igns [x] Reset Hunk')
+      map('<leader>gsp', gs.preview_hunk, '[G]it[s]igns [P]review hunk')
+      map('<leader>gs]', gs.next_hunk, '[G]it[s]igns ] next hunk')
+      map('<leader>gs[', gs.prev_hunk, '[G]it[s]igns [ previous hunk')
+      map('<leader>tl', gs.toggle_current_line_blame, '[T]oggle [L]ine blame')
     end,
   },
 }
