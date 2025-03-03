ddb99a20ea844e4617f972421550a2526fea1b7e
Ziv Zerr <downzed@gmail.com>
2025-03-03 12:26:16 +0100

    feat: missing git toggle line

--- .config/nvim/lua/downzed/plugins/git.lua
+++ .config/nvim/lua/downzed/plugins/git.lua
@@ -1,7 +1,3 @@
-local map = function(key, cmd, desc)
-  vim.keymap.set('n', key, cmd, { silent = true, desc = 'Git: ' .. desc })
-end
-
 return {
   'tpope/vim-fugitive',
   {
@@ -18,15 +14,26 @@ return {
           changedelete = { text = '~' },
           untrack = { text = '·' },
         },
-      })
 
-      map('<leader>gs', '<cmd>Gitsigns<cr>', '[G]it [S]igns')
-      map('<leader>gf', '<cmd>Git<cr>', '[G]it [F]ugitive')
-      map('<leader>gss', gs.stage_hunk, '[G]it[s]igns [S]tage Hunk')
-      map('<leader>gsx', gs.reset_hunk, '[G]it[s]igns [x] Reset Hunk')
-      map('<leader>gsp', gs.preview_hunk, '[G]it[s]igns [P]review hunk')
-      map('<leader>gs]', gs.next_hunk, '[G]it[s]igns ] next hunk')
-      map('<leader>gs[', gs.prev_hunk, '[G]it[s]igns [ previous hunk')
+        on_attach = function(bufnr)
+          local map = function(key, cmd, desc)
+            vim.keymap.set('n', key, cmd, {
+              silent = true,
+              desc = 'Git: ' .. desc,
+              buffer = bufnr,
+            })
+          end
+
+          map('<leader>gs', '<cmd>Gitsigns<cr>', '[G]it [S]igns')
+          map('<leader>gf', '<cmd>Git<cr>', '[G]it [F]ugitive')
+          map('<leader>gss', gs.stage_hunk, '[G]it[s]igns [S]tage Hunk')
+          map('<leader>gsx', gs.reset_hunk, '[G]it[s]igns [x] Reset Hunk')
+          map('<leader>gsp', gs.preview_hunk, '[G]it[s]igns [P]review hunk')
+          map('<leader>gs]', gs.next_hunk, '[G]it[s]igns ] next hunk')
+          map('<leader>gs[', gs.prev_hunk, '[G]it[s]igns [ previous hunk')
+          map('<leader>tl', gs.toggle_current_line_blame, '[T]oggle [L]ine blame')
+        end,
+      })
     end,
   },
 }
